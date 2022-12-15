efdb0dff857674b3345eca89cdb4a342d43d255f
Rob Sokolowski <rpsoko@gmail.com>
2022-12-15 13:16:49 -0500

    vendor elm-editor, to expose EditorModel

--- src/Pages/Stories/TextEditor.elm
+++ src/Pages/Stories/TextEditor.elm
@@ -1,12 +1,20 @@
 module Pages.Stories.TextEditor exposing (Model, Msg, page)
 
+import Editor exposing (Editor)
+import EditorModel
 import Effect exposing (Effect)
+import Element as E exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import Element.Font as Font
+import Element.Input as Input
 import Gen.Params.Stories.TextEditor exposing (Params)
+import Html
 import Page
 import Request
 import Shared
 import View exposing (View)
-import Page
 
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
@@ -21,15 +29,43 @@ page shared req =
 
 
 -- INIT
+-- MODEL
 
 
 type alias Model =
-    {}
+    { editor : Editor
+    }
 
 
-init : ( Model, Effect Msg )
-init =
-    ( {}, Effect.none )
+text : String
+text =
+    "Hello, it is me you're looking for!"
+
+
+config : EditorModel.Config
+config =
+    { width = windowWidth 1800 -- 1200
+    , height = windowHeight 700
+    , fontSize = 16
+    , verticalScrollOffset = 3
+    , viewMode = EditorModel.Dark
+    , debugOn = False
+    , viewLineNumbersOn = False
+    , wrapOption = DontWrap
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    let
+        newEditor =
+            Editor.initWithContent text Load.config
+
+        -- Editor.initWithContent Text.test1 Load.config
+    in
+    ( { editor = newEditor }
+    , Cmd.none
+    )
 
 
 
@@ -37,14 +73,18 @@ init =
 
 
 type Msg
-    = ReplaceMe
+    = MyEditorMsg Editor.EditorMsg
 
 
-update : Msg -> Model -> ( Model, Effect Msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            ( model, Effect.none )
+        MyEditorMsg editorMsg ->
+            let
+                ( newEditor, cmd ) =
+                    Editor.update editorMsg model.editor
+            in
+            ( { model | editor = newEditor }, Cmd.map MyEditorMsg cmd )
 
 
 
@@ -62,4 +102,16 @@ subscriptions model =
 
 view : Model -> View Msg
 view model =
-    View.placeholder "Stories.TextEditor"
+    { title = "Story - Text Editor"
+    , body = viewEditor model
+    }
+
+
+viewElements : Model -> Element Msg
+viewElements model =
+    E.none
+
+
+viewEditor : Model -> Element Msg
+viewEditor model =
+    Editor.view model.editor |> Html.map MyEditorMsg |> E.html

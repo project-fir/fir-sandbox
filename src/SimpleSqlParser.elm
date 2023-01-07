d5d5ad6436306688cf131dc44ef82a114e647853
Rob Sokolowski <rpsoko@gmail.com>
2023-01-07 08:50:44 -0500

    begin parser fun

--- src/SimpleSqlParser.elm
+++ src/SimpleSqlParser.elm
@@ -3,6 +3,7 @@ module SimpleSqlParser exposing (parseRefsFromSql)
 -- I'm not sure if this module is worthy of the word "parser", but all I need is basically functionality
 
 import FirApi exposing (DuckDbRef)
+import Parser as P exposing ((|.), (|=))
 
 
 type alias SqlQuery =
@@ -11,4 +12,44 @@ type alias SqlQuery =
 
 parseRefsFromSql : SqlQuery -> List DuckDbRef
 parseRefsFromSql queryStr =
-    []
+    case P.run fromClause queryStr of
+        Ok value ->
+            [ value ]
+
+        Err error ->
+            []
+
+
+fromClause : P.Parser DuckDbRef
+fromClause =
+    P.succeed DuckDbRef
+        |. P.keyword "from"
+        |. P.spaces
+        |= P.getChompedString (P.chompWhile Char.isAlphaNum)
+        |. P.symbol "."
+        |= P.getChompedString (P.chompWhile Char.isAlphaNum)
+        |. P.spaces
+
+
+joinUsingClause : P.Parser DuckDbRef
+joinUsingClause =
+    P.succeed DuckDbRef
+        |. P.keyword "join"
+        |. P.spaces
+        |= P.getChompedString (P.chompWhile Char.isAlphaNum)
+        |. P.symbol "."
+        |= P.getChompedString (P.chompWhile Char.isAlphaNum)
+        |. P.spaces
+        |. P.keyword "using"
+
+
+joinOnClause : P.Parser DuckDbRef
+joinOnClause =
+    P.succeed DuckDbRef
+        |. P.keyword "join"
+        |. P.spaces
+        |= P.getChompedString (P.chompWhile Char.isAlphaNum)
+        |. P.symbol "."
+        |= P.getChompedString (P.chompWhile Char.isAlphaNum)
+        |. P.spaces
+        |. P.keyword "on"
--- tests/SimpleSqlParserTest.elm
+++ tests/SimpleSqlParserTest.elm
@@ -10,12 +10,52 @@ suite : Test
 suite =
     describe "SimpleSqlParser"
         [ describe "pluck out duckdb refs from query"
-            [ test "case 1"
+            [ test "trivial case"
                 (\_ ->
-                    parseRefsFromSql "select * from foo.bar"
-                        -- TODO: Implement things!
-                        --|> Expect.equal [ { schemaName = "foo", tableName = "bar" } ]
+                    parseRefsFromSql ""
                         |> Expect.equal []
+                )
+            , test "case 1"
+                (\_ ->
+                    parseRefsFromSql "select from foo.bar "
+                        |> Expect.equal [ { schemaName = "foo", tableName = "bar" } ]
+                )
+            , test "case 2"
+                (\_ ->
+                    parseRefsFromSql "select * from foo.bar b"
+                        |> Expect.equal [ { schemaName = "foo", tableName = "bar" } ]
+                )
+            , test "case 3"
+                (\_ ->
+                    parseRefsFromSql "select * from foo.bar join foo.bar2 using bar_id"
+                        |> Expect.equal
+                            [ { schemaName = "foo", tableName = "bar" }
+                            , { schemaName = "foo", tableName = "bar2" }
+                            ]
+                )
+            , test "case 4"
+                (\_ ->
+                    parseRefsFromSql "select * from foo.bar b1 join foo.bar2 using bar_id"
+                        |> Expect.equal
+                            [ { schemaName = "foo", tableName = "bar" }
+                            , { schemaName = "foo", tableName = "bar2" }
+                            ]
+                )
+            , test "case 5"
+                (\_ ->
+                    parseRefsFromSql "select * from foo.bar b1 join foo.bar2 b2 using bar_id"
+                        |> Expect.equal
+                            [ { schemaName = "foo", tableName = "bar" }
+                            , { schemaName = "foo", tableName = "bar2" }
+                            ]
+                )
+            , test "case 6"
+                (\_ ->
+                    parseRefsFromSql "select * from foo.bar b1 join foo.bar2 b2 on b1.bar_id = b2.bar_id"
+                        |> Expect.equal
+                            [ { schemaName = "foo", tableName = "bar" }
+                            , { schemaName = "foo", tableName = "bar2" }
+                            ]
                 )
             ]
         ]
